1b733d05013d5b1b2ec9c6dae385f290eb1a50cc
Sristi Agrawal <sristiagrawal2208@gmail.com>
2021-03-13 03:06:32 +0530

    Django integration - 2

--- tutionfinder/guruhub/views.py
+++ tutionfinder/guruhub/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Card
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    car = Card.objects.all()
+    return render(request, 'index.html', {'cards':car})
