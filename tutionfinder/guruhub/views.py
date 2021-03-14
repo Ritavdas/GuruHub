d4d159267878e7c33327e33b0d99755c4addccdd
harshyp47 <harshyp47@gmail.com>
2021-03-14 17:29:36 +0530

    Search Feature Done! Without handling exceptions!

--- tutionfinder/guruhub/views.py
+++ tutionfinder/guruhub/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Card, Subject, Tutor
+
 # Create your views here.
 def index(request):
     car = Card.objects.all()
@@ -8,5 +9,25 @@ def index(request):
 
 
 def searchresults(request):
+
+    sub = request.GET['subjects']
+    level = request.GET['level']
+    addr = request.GET['location']
+
+   
+
+    try:
+        mode = bool(request.GET['mode'])
+        mode = 'In Person'
+
+    except:
+        mode = 'Online'
+
+ 
+    results = Subject.objects.filter(subject__icontains = sub, level__icontains = level, mode__icontains = mode).select_related('tid')
+
     
-    return render(request, 'searchresults.html')
+
+    
+
+    return render(request, 'searchresults.html',{'results':results})
