631264b5928366e9451d4ab12b2205b71b744e22
ritavdas <ritavdas@gmail.com>
2021-03-28 23:40:41 +0530

    Restore

--- Payment/stripe_example/example/models.py
+++ Payment/stripe_example/example/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Teachers(models.Model):
-    name = models.CharField(max_length=200)
-    img = models.ImageField(upload_to="pics")
-    desc = models.TextField()
-    price = models.IntegerField()
-
-    class Meta:
-        db_table = "Teach"
--- tutionfinder/guruhub/admin.py
+++ tutionfinder/guruhub/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Card
+from .models import Card, Tutor, Subject
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Tutor)
+admin.site.register(Subject)
