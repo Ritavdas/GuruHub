7104632db95126425fe50e3ff3a01f5f199c03a0
ritavdas <ritavdas@gmail.com>
2021-04-28 18:09:35 +0530

    Payments made dynamic

--- tutionfinder/guruhub/models.py
+++ tutionfinder/guruhub/models.py
@@ -3,46 +3,36 @@ from django.contrib.auth.models import User, auth
 
 # Create your models here.
 
-class Card(models.Model):
-   img = models.ImageField(upload_to='pics')
-   name = models.CharField(max_length=20)
-   desc = models.TextField()
 
+class Card(models.Model):
+    img = models.ImageField(upload_to='pics')
+    name = models.CharField(max_length=20)
+    desc = models.TextField()
 
 
 class Tutor(models.Model):
-   tid = models.IntegerField(primary_key = True)
-   pic = models.ImageField(upload_to='pics',blank = True)
-   name = models.CharField(max_length=50,blank = True)
-   star = models.IntegerField(blank = True)
-   about = models.CharField(max_length=200,blank = True)
-   taught = models.IntegerField(blank = True)
-   rate = models.IntegerField(blank = True)
-   address = models.CharField(max_length=50,blank = True)
-
+    tid = models.IntegerField(primary_key=True)
+    pic = models.ImageField(upload_to='pics', blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    star = models.IntegerField(blank=True)
+    about = models.CharField(max_length=200, blank=True)
+    taught = models.IntegerField(blank=True)
+    rate = models.IntegerField(blank=True)
+    address = models.CharField(max_length=50, blank=True)
+    num = models.IntegerField()
 
 
 class Subject(models.Model):
 
-   tid = models.ForeignKey(Tutor, on_delete=models.CASCADE)
-   subject = models.CharField(max_length=20)
-   level = models.CharField(max_length=20)
-   mode = models.CharField(max_length=20)
+    tid = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=20)
+    level = models.CharField(max_length=20)
+    mode = models.CharField(max_length=20)
 
 
 class Register(models.Model):
-   first_name = models.CharField(max_length=50)
-   last_name = models.CharField(max_length=50)
-   user_name = models.CharField(max_length=50)
-   password= models.CharField(max_length=50)
-   email = models.EmailField(max_length=50)
-   
-
-
-
-
-
-
-
-
-
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
--- tutionfinder/guruhub/views.py
+++ tutionfinder/guruhub/views.py
@@ -1,3 +1,4 @@
+from .models import Card
 from django.shortcuts import render, redirect
 from .models import Card, Subject, Tutor
 from hashlib import sha1
@@ -8,9 +9,11 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth.hashers import check_password, make_password
 
 # Create your views here.
+
+
 def index(request):
     car = Card.objects.all()
-    return render(request, 'index.html', {'cards':car})
+    return render(request, 'index.html', {'cards': car})
 
 
 def regres(request):
@@ -23,56 +26,56 @@ def regres(request):
     confirm_password = request.POST['password2']
     category = request.POST['category']
 
-    if fn=="" or username=="" or email=="" or password=="" or confirm_password=="":
+    if fn == "" or username == "" or email == "" or password == "" or confirm_password == "":
 
-        return render(request, 'index.html',{'msg':"Please fill required field!"})
+        return render(request, 'index.html', {'msg': "Please fill required field!"})
 
-       
     if password != confirm_password:
-        return render(request, 'index.html',{'msg':'Password do not match!'})
+        return render(request, 'index.html', {'msg': 'Password do not match!'})
     if User.objects.filter(username=username).exists():
-        return render(request, 'index.html',{'msg':'Username already Exists!'})
+        return render(request, 'index.html', {'msg': 'Username already Exists!'})
 
     else:
         if User.objects.filter(email=email).exists():
-            return render(request, 'index.html',{'msg':'Email already Exists!'})
+            return render(request, 'index.html', {'msg': 'Email already Exists!'})
 
         else:
-            #Looks good
+            # Looks good
             #User.objects.create(first_name=fn, last_name = ln, username = username, password = password,email = email)
-            user = User.objects.create_user(first_name=fn, last_name = ln, username = username, password = password,email = email)
+            user = User.objects.create_user(
+                first_name=fn, last_name=ln, username=username, password=password, email=email)
             user.save()
 
             if category == "tutor":
-                Tutor(tid = int(Tutor.objects.all().count()) + 1, pic = None, name = fn, star = 0, about = "null", taught = 0, rate = 0, address = "null" ).save()
-            return render(request, 'login.html',{'msg1':"Thanks for Registering with us."})
+                Tutor(tid=int(Tutor.objects.all().count()) + 1, pic=None, name=fn,
+                      star=0, about="null", taught=0, rate=0, address="null").save()
+            return render(request, 'login.html', {'msg1': "Thanks for Registering with us."})
+
 
 def loginload(request):
-    return render(request,'login.html')
+    return render(request, 'login.html')
 
 
 def logout(request):
-    
+
     auth.logout(request)
-    return render(request, 'index.html',{'msg1':"Successfully logged out!"})
-
-
+    return render(request, 'index.html', {'msg1': "Successfully logged out!"})
 
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username'] 
+        username = request.POST['username']
         password = request.POST['password1']
 
         user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
-            return render(request, 'index.html',{'msg1':"You are now logged in"})
+            return render(request, 'index.html', {'msg1': "You are now logged in"})
         else:
-            return render(request, 'login.html',{'msg2':"Invalid Credentials"})
+            return render(request, 'login.html', {'msg2': "Invalid Credentials"})
     else:
-        return render(request,'login.html')
+        return render(request, 'login.html')
 
 
 def searchresults(request):
@@ -81,8 +84,6 @@ def searchresults(request):
     level = request.GET['level']
     addr = request.GET['location']
 
-   
-
     try:
         mode = bool(request.GET['mode'])
         mode = 'In Person'
@@ -90,22 +91,17 @@ def searchresults(request):
     except:
         mode = 'Online'
 
- 
-    results = Subject.objects.filter(subject__icontains = sub, level__icontains = level, mode__icontains = mode).select_related('tid')
+    results = Subject.objects.filter(
+        subject__icontains=sub, level__icontains=level, mode__icontains=mode).select_related('tid')
 
-    
+    return render(request, 'searchresults.html', {'results': results})
 
-    
-
-    return render(request, 'searchresults.html',{'results':results})
-
-
-from .models import Card
 
 # Create your views here.
+
 def index(request):
     car = Card.objects.all()
-    return render(request, 'index.html', {'cards':car})
+    return render(request, 'index.html', {'cards': car})
 
 
 def maindash(request):
@@ -116,64 +112,51 @@ def maindash(request):
 def changepswd(request):
     return render(request, 'changepswd.html')
 
+
 def pswdchanged(request):
     msg = ""
-
-
 
     old = request.POST['old']
     new = request.POST['new']
     connew = request.POST['connew']
 
-    if old == "" or new=="" or connew=="":
-        return render(request,'changepswd.html', {'msg':"Enter required fields!"})
+    if old == "" or new == "" or connew == "":
+        return render(request, 'changepswd.html', {'msg': "Enter required fields!"})
 
-    
-    if new!=connew:
-        return render(request,'changepswd.html', {'msg':"Password do not match!"})
+    if new != connew:
+        return render(request, 'changepswd.html', {'msg': "Password do not match!"})
 
-    if check_password(old,User.objects.get(username = request.user.username).password):
-        p = User.objects.get(username = request.user.username)
+    if check_password(old, User.objects.get(username=request.user.username).password):
+        p = User.objects.get(username=request.user.username)
         p.password = make_password(new)
         p.save()
-        send_mail('Password changed!', 'Hello ' + request.user.first_name + ", Your password has been changed successfully!", 'guruhubportal@gmail.com', [request.user.email],fail_silently=False)
-        return render(request, 'pswdchanged.html', {'msg':"Password successfully changed! Mail sent to respective mailing address!"})
+        send_mail('Password changed!', 'Hello ' + request.user.first_name + ", Your password has been changed successfully!",
+                  'guruhubportal@gmail.com', [request.user.email], fail_silently=False)
+        return render(request, 'pswdchanged.html', {'msg': "Password successfully changed! Mail sent to respective mailing address!"})
     else:
-        return render(request,'changepswd.html', {'msg':"Old password do not match with our records!"})
+        return render(request, 'changepswd.html', {'msg': "Old password do not match with our records!"})
 
 
 def forgetpass(request):
-    return render(request,'forgetpass.html')
+    return render(request, 'forgetpass.html')
 
 
 def mailsent(request):
 
-    pwds = ['1234', 'samarth', 'capstone', '1111', 'mypassword','hello']
+    pwds = ['1234', 'samarth', 'capstone', '1111', 'mypassword', 'hello']
 
-    r = random.randint(0,len(pwds)-1)
-
-
+    r = random.randint(0, len(pwds)-1)
 
     username = request.GET['username']
-    
+
     if User.objects.filter(username=username).exists():
-        email = User.objects.get(username = username).email
-        p = User.objects.get(username = username)
+        email = User.objects.get(username=username).email
+        p = User.objects.get(username=username)
         p.password = make_password(pwds[r])
         p.save()
-        send_mail('Password changed!', 'Hello ' + username + ", /n Your new password is "+"'"+pwds[r]+"'", 'guruhubportal@gmail.com', [email],fail_silently=False)
-        return render(request,'mailsent.html')
+        send_mail('Password changed!', 'Hello ' + username + ", /n Your new password is " +
+                  "'"+pwds[r]+"'", 'guruhubportal@gmail.com', [email], fail_silently=False)
+        return render(request, 'mailsent.html')
 
     else:
-        return render(request,'forgetpass.html', {"msg":"User does not exist!"})
-
-    
-    
-
-
-
-
-    
-    
-
-
+        return render(request, 'forgetpass.html', {"msg": "User does not exist!"})
